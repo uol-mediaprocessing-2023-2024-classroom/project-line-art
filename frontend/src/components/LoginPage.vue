224dee6d2ca6e292157b9453b0123512a2b9a720
TaleaSch12 <talea.schweers@uni-oldenburg.de>
2024-01-28 21:32:53 +0100

    feat: add enter function to login

--- frontend/src/components/LoginPage.vue
+++ frontend/src/components/LoginPage.vue
@@ -5,7 +5,7 @@
             <h2>E-mail</h2>
             <input required  v-model="loginData.email" type="email" name="email" autocomplete="email" />
             <h2>Passwort:</h2>
-            <input required  v-model="loginData.password" type="password" name="password" autocomplete="password" /> 
+            <input required  v-model="loginData.password" type="password" name="password" autocomplete="password" @keyup.enter="login" /> 
             <v-btn @click="login" :disabled="awaitingLoginResponse" class="loginButton">
                 <v-progress-circular indeterminate color="grey lighten-5" v-if="awaitingLoginResponse"></v-progress-circular>
                 <div v-else>
